3f17f33357ba905c20709d7393777f6fd3297510
asmit27rai <raiasmit10@gmail.com>
2025-06-11 19:22:23 +0530

    Kubebuilder

---
+++ test/performance/kubestellar-latency-collector/cmd/main.go
@@ -0,0 +1,218 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"os"
+	"time"
+
+	"github.com/spf13/pflag"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/dynamic"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	"github.com/kubestellar/kubestellar-latency-collector/controllers"
+)
+
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+}
+
+func main() {
+	var (
+		metricsAddr          string
+		enableLeaderElection bool
+		probeAddr            string
+		secureMetrics        bool
+		enableHTTP2          bool
+		
+		// Context flags
+		wdsContext          string
+		itsContext          string
+		wecContext          string
+		kubeconfigPath      string
+		monitoredNamespace  string
+		monitoredDeployment string
+		bindingName         string
+	)
+
+	// Add WDS context flag
+	pflag.StringVar(&wdsContext, "wds-context", "", "Context name for WDS cluster in kubeconfig")
+	pflag.StringVar(&itsContext, "its-context", "", "Context name for ITS cluster in kubeconfig")
+	pflag.StringVar(&wecContext, "wec-context", "", "Context name for WEC cluster in kubeconfig")
+	pflag.StringVar(&kubeconfigPath, "kubeconfig", "", "Path to kubeconfig file")
+	pflag.StringVar(&monitoredNamespace, "monitored-namespace", "default", "Namespace of the deployment to monitor")
+	pflag.StringVar(&monitoredDeployment, "monitored-deployment", "", "Name of the deployment to monitor")
+	pflag.StringVar(&bindingName, "binding-name", "", "Name of the binding policy for the monitored deployment")
+	
+	// Existing flags
+	pflag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	pflag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	pflag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
+	pflag.BoolVar(&secureMetrics, "metrics-secure", false,
+		"If set the metrics endpoint is served securely")
+	pflag.BoolVar(&enableHTTP2, "enable-http2", false,
+		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
+
+	opts := zap.Options{
+		Development: true,
+	}
+	opts.BindFlags(flag.CommandLine)
+	pflag.Parse()
+
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	// Disable HTTP/2 if not enabled
+	disableHTTP2 := func(c *tls.Config) {
+		setupLog.Info("disabling http/2")
+		c.NextProtos = []string{"http/1.1"}
+	}
+
+	tlsOpts := []func(*tls.Config){}
+	if !enableHTTP2 {
+		tlsOpts = append(tlsOpts, disableHTTP2)
+	}
+
+	webhookServer := webhook.NewServer(webhook.Options{
+		TLSOpts: tlsOpts,
+	})
+
+	// Create manager
+	mgrCfg := buildClusterConfig(kubeconfigPath, wdsContext)
+	mgr, err := ctrl.NewManager(mgrCfg, ctrl.Options{
+		Scheme: scheme,
+		Metrics: metricsserver.Options{
+			BindAddress:   metricsAddr,
+			SecureServing: secureMetrics,
+			TLSOpts:       tlsOpts,
+		},
+		WebhookServer:          webhookServer,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "34020a28.kubestellar.io",
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	// Build clients for all clusters
+	wdsCfg := buildClusterConfig(kubeconfigPath, wdsContext)
+	itsCfg := buildClusterConfig(kubeconfigPath, itsContext)
+	wecCfg := buildClusterConfig(kubeconfigPath, wecContext)
+	
+	wdsClient, err := kubernetes.NewForConfig(wdsCfg)
+	if err != nil {
+		setupLog.Error(err, "unable to create WDS client")
+		os.Exit(1)
+	}
+	
+	wdsDynamic, err := dynamic.NewForConfig(wdsCfg)
+	if err != nil {
+		setupLog.Error(err, "unable to create WDS dynamic client")
+		os.Exit(1)
+	}
+	
+	wecClient, err := kubernetes.NewForConfig(wecCfg)
+	if err != nil {
+		setupLog.Error(err, "unable to create WEC client")
+		os.Exit(1)
+	}
+	
+	wecDynamic, err := dynamic.NewForConfig(wecCfg)
+	if err != nil {
+		setupLog.Error(err, "unable to create WEC dynamic client")
+		os.Exit(1)
+	}
+	
+	itsDynamic, err := dynamic.NewForConfig(itsCfg)
+	if err != nil {
+		setupLog.Error(err, "unable to create ITS dynamic client")
+		os.Exit(1)
+	}
+
+	// Initialize Reconciler with dependencies
+	if err = (&controllers.LatencyCollectorReconciler{
+		Client:              mgr.GetClient(),
+		Scheme:              mgr.GetScheme(),
+		WdsClient:           wdsClient,
+		WecClient:           wecClient,
+		WdsDynamic:          wdsDynamic,
+		ItsDynamic:          itsDynamic,
+		WecDynamic:          wecDynamic,
+		MonitoredNamespace:  monitoredNamespace,
+		MonitoredDeployment: monitoredDeployment,
+		BindingName:         bindingName,
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Deployment")
+		os.Exit(1)
+	}
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
+
+func buildClusterConfig(kubeconfigPath, context string) *rest.Config {
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	loadingRules.ExplicitPath = kubeconfigPath
+
+	overrides := &clientcmd.ConfigOverrides{}
+	if context != "" {
+		overrides.CurrentContext = context
+	}
+
+	config, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		loadingRules, overrides).ClientConfig()
+	if err != nil {
+		setupLog.Error(err, "unable to create REST config", "context", context)
+		os.Exit(1)
+	}
+	
+	// Set reasonable timeouts
+	config.Timeout = 15 * time.Second
+	return config
+}
---
+++ test/performance/kubestellar-latency-collector/controllers/deployment_controller.go
@@ -0,0 +1,428 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
+)
+
+const workStatusSuffixFmt = "appsv1-deployment-%s-%s"
+
+// LatencyCollectorReconciler reconciles a Deployment object
+type LatencyCollectorReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+
+	// Clients
+	WdsClient  kubernetes.Interface
+	WecClient  kubernetes.Interface
+	WdsDynamic dynamic.Interface
+	ItsDynamic dynamic.Interface
+	WecDynamic dynamic.Interface
+
+	// Configuration
+	MonitoredNamespace  string
+	MonitoredDeployment string
+	BindingName         string
+
+	// Cache
+	cache     LatencyCache
+	cacheLock sync.Mutex
+
+	// Metrics
+	downsyncBindingTime        *prometheus.GaugeVec
+	downsyncPackagingTime      *prometheus.GaugeVec
+	downsyncDeliveryTime       *prometheus.GaugeVec
+	downsyncActivationTime     *prometheus.GaugeVec
+	totalDownsyncTime          *prometheus.GaugeVec
+	upsyncReportTime           *prometheus.GaugeVec
+	upsyncFinalizationTime     *prometheus.GaugeVec
+	totalUpsyncTime            *prometheus.GaugeVec
+	e2eLatencyTime             *prometheus.GaugeVec
+}
+
+type LatencyCache struct {
+	bindingCreated            time.Time
+	wdsDeploymentCreated      time.Time
+	wdsDeploymentStatusTime   time.Time
+	manifestWorkCreated       time.Time
+	appliedManifestWorkCreated time.Time
+	wecDeploymentCreated      time.Time
+	wecDeploymentStatusTime   time.Time
+	workStatusTime            time.Time
+}
+
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch
+//+kubebuilder:rbac:groups=control.kubestellar.io,resources=bindingpolicies;workstatuses,verbs=get;list
+//+kubebuilder:rbac:groups=work.open-cluster-management.io,resources=manifestworks;appliedmanifestworks,verbs=get;list
+
+func (r *LatencyCollectorReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	r.registerMetrics()
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&appsv1.Deployment{}).
+		Complete(r)
+}
+
+func (r *LatencyCollectorReconciler) registerMetrics() {
+	labels := []string{"namespace", "deployment"}
+	
+	r.downsyncBindingTime = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "kubestellar_downsync_binding_time_seconds",
+		Help: "Time from binding creation to WDS deployment creation",
+	}, labels)
+	
+	r.downsyncPackagingTime = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "kubestellar_downsync_packaging_time_seconds",
+		Help: "Time from WDS deployment creation to ManifestWork creation",
+	}, labels)
+	
+	r.downsyncDeliveryTime = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "kubestellar_downsync_delivery_time_seconds",
+		Help: "Time from ManifestWork creation to AppliedManifestWork creation",
+	}, labels)
+	
+	r.downsyncActivationTime = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "kubestellar_downsync_activation_time_seconds",
+		Help: "Time from AppliedManifestWork creation to WEC deployment creation",
+	}, labels)
+	
+	r.totalDownsyncTime = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "kubestellar_total_downsync_time_seconds",
+		Help: "Total downsync time from WDS deployment creation to WEC deployment creation",
+	}, labels)
+	
+	r.upsyncReportTime = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "kubestellar_upsync_report_time_seconds",
+		Help: "Time from WEC status update to WorkStatus update",
+	}, labels)
+	
+	r.upsyncFinalizationTime = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "kubestellar_upsync_finalization_time_seconds",
+		Help: "Time from WorkStatus update to WDS status update",
+	}, labels)
+	
+	r.totalUpsyncTime = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "kubestellar_total_upsync_time_seconds",
+		Help: "Total upsync time from WEC status update to WDS status update",
+	}, labels)
+	
+	r.e2eLatencyTime = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "kubestellar_e2e_latency_seconds",
+		Help: "End-to-end latency from WDS creation to status update",
+	}, labels)
+
+	metrics.Registry.MustRegister(
+		r.downsyncBindingTime,
+		r.downsyncPackagingTime,
+		r.downsyncDeliveryTime,
+		r.downsyncActivationTime,
+		r.totalDownsyncTime,
+		r.upsyncReportTime,
+		r.upsyncFinalizationTime,
+		r.totalUpsyncTime,
+		r.e2eLatencyTime,
+	)
+}
+
+func (r *LatencyCollectorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	logger := log.FromContext(ctx)
+	
+	// Skip if not our monitored deployment
+	if req.NamespacedName.Namespace != r.MonitoredNamespace || 
+	   req.NamespacedName.Name != r.MonitoredDeployment {
+		return ctrl.Result{}, nil
+	}
+
+	// Lock cache for thread safety
+	r.cacheLock.Lock()
+	defer r.cacheLock.Unlock()
+
+	// Fetch WDS Deployment
+	wdsDeploy := &appsv1.Deployment{}
+	if err := r.Get(ctx, req.NamespacedName, wdsDeploy); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// Update cache
+	r.updateCacheFromDeployment(wdsDeploy)
+	
+	// Fetch BindingPolicy
+	if r.cache.bindingCreated.IsZero() {
+		if err := r.fetchBindingPolicy(ctx); err != nil {
+			logger.Error(err, "Failed to fetch BindingPolicy")
+		}
+	}
+
+	// Fetch ManifestWork
+	if r.cache.manifestWorkCreated.IsZero() {
+		if err := r.fetchManifestWork(ctx); err != nil {
+			logger.Error(err, "Failed to fetch ManifestWork")
+		}
+	}
+
+	// Fetch AppliedManifestWork
+	if !r.cache.manifestWorkCreated.IsZero() && r.cache.appliedManifestWorkCreated.IsZero() {
+		if err := r.fetchAppliedManifestWork(ctx); err != nil {
+			logger.Error(err, "Failed to fetch AppliedManifestWork")
+		}
+	}
+
+	// Fetch WorkStatus
+	if r.cache.workStatusTime.IsZero() {
+		if err := r.fetchWorkStatus(ctx); err != nil {
+			logger.Error(err, "Failed to fetch WorkStatus")
+		}
+	}
+
+	// Fetch WEC Deployment
+	if r.cache.wecDeploymentCreated.IsZero() || r.cache.wecDeploymentStatusTime.IsZero() {
+		if err := r.fetchWECDeployment(ctx); err != nil {
+			logger.Error(err, "Failed to fetch WEC Deployment")
+		}
+	}
+
+	// Compute and expose metrics
+	r.computeMetrics()
+
+	return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
+}
+
+func (r *LatencyCollectorReconciler) updateCacheFromDeployment(wdsDeploy *appsv1.Deployment) {
+	if r.cache.wdsDeploymentCreated.IsZero() {
+		r.cache.wdsDeploymentCreated = wdsDeploy.CreationTimestamp.Time
+		log.FromContext(context.TODO()).Info("🛠️  WDS Deployment timestamp",
+			"wdsDeploymentCreated", r.cache.wdsDeploymentCreated,
+		)
+	}
+	
+	if statusTime := getDeploymentStatusTime(wdsDeploy); !statusTime.IsZero() {
+		r.cache.wdsDeploymentStatusTime = statusTime
+		log.FromContext(context.TODO()).Info("📈  WDS Deployment status timestamp",
+			"wdsDeploymentStatusTime", r.cache.wdsDeploymentStatusTime,
+		)
+	}
+}
+
+func (r *LatencyCollectorReconciler) fetchBindingPolicy(ctx context.Context) error {
+	bindingGVR := schema.GroupVersionResource{
+		Group:    "control.kubestellar.io",
+		Version:  "v1alpha1",
+		Resource: "bindingpolicies",
+	}
+	binding, err := r.WdsDynamic.Resource(bindingGVR).Get(ctx, r.BindingName, metav1.GetOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to get binding policy %s: %w", r.BindingName, err)
+	}
+	
+	r.cache.bindingCreated = binding.GetCreationTimestamp().Time
+	log.FromContext(ctx).Info("🔖  BindingPolicy timestamp",
+       	"bindingCreated", r.cache.bindingCreated,
+   	)
+	return nil
+}
+
+
+func (r *LatencyCollectorReconciler) fetchManifestWork(ctx context.Context) error {
+	manifestWorkGVR := schema.GroupVersionResource{
+		Group:    "work.open-cluster-management.io",
+		Version:  "v1",
+		Resource: "manifestworks",
+	}
+	
+	labelSelector := fmt.Sprintf("transport.kubestellar.io/originOwnerReferenceBindingKey=%s", r.BindingName)
+	manifestWorkList, err := r.ItsDynamic.Resource(manifestWorkGVR).List(ctx, metav1.ListOptions{
+		LabelSelector: labelSelector,
+	})
+
+	fmt.Printf("Found %d ManifestWorks\n", len(manifestWorkList.Items))
+	
+	if err != nil || len(manifestWorkList.Items) == 0 {
+		return fmt.Errorf("failed to get ManifestWork for binding %s: %w", r.BindingName, err)
+	}
+	
+	// Find ManifestWork that matches our deployment
+	for _, mw := range manifestWorkList.Items {
+		manifests, _, _ := unstructured.NestedSlice(mw.Object, "spec", "workload", "manifests")
+		for _, manifest := range manifests {
+			if manifestMap, ok := manifest.(map[string]interface{}); ok {
+				if kind, _, _ := unstructured.NestedString(manifestMap, "kind"); kind == "Deployment" {
+					if name, _, _ := unstructured.NestedString(manifestMap, "metadata", "name"); name == r.MonitoredDeployment {
+						r.cache.manifestWorkCreated = mw.GetCreationTimestamp().Time
+						log.FromContext(ctx).Info("📦  ManifestWork timestamp",
+							"manifestWorkCreated", r.cache.manifestWorkCreated,
+						)
+						return nil
+					}
+				}
+			}
+		}
+	}
+	
+	return fmt.Errorf("no matching ManifestWork found for deployment %s", r.MonitoredDeployment)
+}
+
+func (r *LatencyCollectorReconciler) fetchAppliedManifestWork(ctx context.Context) error {
+	appliedManifestWorkGVR := schema.GroupVersionResource{
+		Group:    "work.open-cluster-management.io",
+		Version:  "v1",
+		Resource: "appliedmanifestworks",
+	}
+	
+	// List all AppliedManifestWorks
+	appliedWorkList, err := r.WecDynamic.Resource(appliedManifestWorkGVR).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to list AppliedManifestWorks: %w", err)
+	}
+
+	fmt.Printf("Found %d AppliedManifestWorks\n", len(appliedWorkList.Items))
+	fmt.Printf("AppliedManifestWorkList: %v\n", appliedWorkList)
+	
+	// Find AppliedManifestWork that matches our deployment
+	for _, aw := range appliedWorkList.Items {
+		namespace, _, _ := unstructured.NestedString(aw.Object, "namespace")
+		name, _, _ := unstructured.NestedString(aw.Object, "name")
+		
+		if namespace == r.MonitoredNamespace && name == r.MonitoredDeployment {
+			r.cache.appliedManifestWorkCreated = aw.GetCreationTimestamp().Time
+			log.FromContext(ctx).Info("📄  AppliedManifestWork timestamp",
+				"appliedManifestWorkCreated", r.cache.appliedManifestWorkCreated,
+			)
+			return nil
+		}
+	}
+	
+	return fmt.Errorf("no AppliedManifestWork found for deployment %s/%s", 
+		r.MonitoredNamespace, r.MonitoredDeployment)
+}
+
+func (r *LatencyCollectorReconciler) fetchWorkStatus(ctx context.Context) error {
+	workStatusGVR := schema.GroupVersionResource{
+		Group:    "control.kubestellar.io",
+		Version:  "v1alpha1",
+		Resource: "workstatuses",
+	}
+	
+	workStatusSuffix := fmt.Sprintf(workStatusSuffixFmt, r.MonitoredNamespace, r.MonitoredDeployment)
+	workStatusList, err := r.ItsDynamic.Resource(workStatusGVR).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to list WorkStatuses: %w", err)
+	}
+
+	for _, ws := range workStatusList.Items {
+		if strings.HasSuffix(ws.GetName(), workStatusSuffix) {
+			r.cache.workStatusTime = getStatusTime(&ws)
+			log.FromContext(ctx).Info("📊  WorkStatus timestamp",
+				"workStatusTime", r.cache.workStatusTime,
+			)
+			return nil
+		}
+	}
+	
+	return fmt.Errorf("workstatus not found for suffix %s", workStatusSuffix)
+}
+
+func (r *LatencyCollectorReconciler) fetchWECDeployment(ctx context.Context) error {
+	dep, err := r.WecClient.AppsV1().Deployments(r.MonitoredNamespace).
+		Get(ctx, r.MonitoredDeployment, metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	if r.cache.wecDeploymentCreated.IsZero() {
+		r.cache.wecDeploymentCreated = dep.CreationTimestamp.Time
+		log.FromContext(ctx).Info("🚀  WEC Deployment timestamp",
+			"wecDeploymentCreated", r.cache.wecDeploymentCreated,
+		)
+	}
+	
+	if statusTime := getDeploymentStatusTime(dep); !statusTime.IsZero() {
+		r.cache.wecDeploymentStatusTime = statusTime
+		log.FromContext(ctx).Info("📈  WEC Deployment status timestamp",
+			"wecDeploymentStatusTime", r.cache.wecDeploymentStatusTime,
+		)
+	}
+	return nil
+}
+
+func (r *LatencyCollectorReconciler) computeMetrics() {
+	// Helper function to set metric if timestamp exists
+	setMetric := func(gauge *prometheus.GaugeVec, start, end time.Time) {
+		if !start.IsZero() && !end.IsZero() {
+			gauge.WithLabelValues(
+				r.MonitoredNamespace,
+				r.MonitoredDeployment,
+			).Set(end.Sub(start).Seconds())
+		}
+	}
+
+	// Downsync metrics
+	setMetric(r.downsyncBindingTime, r.cache.bindingCreated, r.cache.wdsDeploymentCreated)
+	setMetric(r.downsyncPackagingTime, r.cache.wdsDeploymentCreated, r.cache.manifestWorkCreated)
+	setMetric(r.downsyncDeliveryTime, r.cache.manifestWorkCreated, r.cache.appliedManifestWorkCreated)
+	setMetric(r.downsyncActivationTime, r.cache.appliedManifestWorkCreated, r.cache.wecDeploymentCreated)
+	setMetric(r.totalDownsyncTime, r.cache.wdsDeploymentCreated, r.cache.wecDeploymentCreated)
+
+	// Upsync metrics
+	if !r.cache.workStatusTime.IsZero() {
+		setMetric(r.upsyncReportTime, r.cache.wecDeploymentStatusTime, r.cache.workStatusTime)
+		setMetric(r.upsyncFinalizationTime, r.cache.workStatusTime, r.cache.wdsDeploymentStatusTime)
+		setMetric(r.totalUpsyncTime, r.cache.wecDeploymentStatusTime, r.cache.wdsDeploymentStatusTime)
+	}
+
+	// E2E latency
+	setMetric(r.e2eLatencyTime, r.cache.wdsDeploymentCreated, r.cache.wdsDeploymentStatusTime)
+}
+
+func getDeploymentStatusTime(dep *appsv1.Deployment) time.Time {
+	var latest time.Time
+	for _, cond := range dep.Status.Conditions {
+		// Use LastUpdateTime instead of LastTransitionTime
+		if !cond.LastUpdateTime.IsZero() && cond.LastUpdateTime.Time.After(latest) {
+			latest = cond.LastUpdateTime.Time
+		}
+	}
+	return latest
+}
+
+func getStatusTime(obj metav1.Object) time.Time {
+	var latestTime time.Time
+	for _, mf := range obj.GetManagedFields() {
+		if mf.Operation == metav1.ManagedFieldsOperationUpdate && mf.Subresource == "status" {
+			if mf.Time != nil && mf.Time.Time.After(latestTime) {
+				latestTime = mf.Time.Time
+			}
+		}
+	}
+	return latestTime
+}
